869f8b47f83fe759b0244a33ccbfe177b8e6f587
natanascimento <natanascimentom@outlook.com>
2021-04-22 03:26:39 -0300

    Definindo novo diretorio para o consumer de valores aleatorios

--- src/generation_random_values/consumer.py
+++ src/generation_random_values/consumer.py
@@ -1,6 +1,11 @@
+import os
+
+import dotenv 
 from kafka import KafkaConsumer
-import os 
+
+dotenv.load_dotenv(dotenv.find_dotenv())
 
 consumer = KafkaConsumer('kafka-python-topic',bootstrap_servers='{}:{}'.format(os.getenv('KAFKA_SERVER'), os.getenv('KAFKA_PORT')))
+
 for message in consumer:
-    print (message)
+    print (message.value.decode('utf-8'))
