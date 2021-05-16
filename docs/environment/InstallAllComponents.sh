b3b9dd9f436a42b845b41b84dccf84cd407a705a
natanascimento <natanascimentom@outlook.com>
2021-05-16 02:43:11 -0300

    Adding README and Updating bash script

--- docs/environment/InstallAllComponents.sh
+++ docs/environment/InstallAllComponents.sh
@@ -19,17 +19,17 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io
 sudo apt-get update
 sudo apt-get install virtualbox
 #Installing Kubectl
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
-echo "$(<kubectl.sha256) kubectl" | sha256sum --check
+sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+sudo echo "$(<kubectl.sha256) kubectl" | sha256sum --check
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-kubectl version --client
+sudo kubectl version --client
 #Installing Minikube
-curl -Lo minikube https://github.com/kubernetes/minikube/releases/download/v0.28.0/minikube-linux-amd64
-chmod +x minikube && mv minikube /usr/local/bin/
+sudo curl -Lo minikube https://github.com/kubernetes/minikube/releases/download/v0.28.0/minikube-linux-amd64
+sudo chmod +x minikube && mv minikube /usr/local/bin/
 #Installing Helm
-curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
+sudo curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
 sudo apt-get install apt-transport-https --yes
-echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
 sudo apt-get update
 sudo apt-get install helm
