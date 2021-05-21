ccf5b78ffd2306a65247cf0af3a7f205206efcda
Natan Nascimento <natanascimentom@outlook.com>
2021-05-20 21:10:03 -0300

    fix: changing input data topic and confluent config library

--- src/PocKafkaConnect/producer.py
+++ src/PocKafkaConnect/producer.py
@@ -1,16 +1,20 @@
-from confluent_kafka import Producer, KafkaError
 import json
-import ccloud_lib
+import time
+
+from confluent_kafka import Producer, KafkaError
+from confluent import ccloud
+
+from mock.customer import CustomerDataProducer
 
 
 if __name__ == '__main__':
 
-    args = ccloud_lib.parse_args()
+    args = ccloud.parse_args()
     config_file = args.config_file
     topic = args.topic
-    conf = ccloud_lib.read_ccloud_config(config_file)
+    conf = ccloud.read_ccloud_config(config_file)
 
-    producer_conf = ccloud_lib.pop_schema_registry_params_from_config(conf)
+    producer_conf = ccloud.pop_schema_registry_params_from_config(conf)
     producer = Producer(producer_conf)
 
     delivered_records = 0
@@ -27,13 +31,19 @@ if __name__ == '__main__':
             print("Produced record to topic {} partition [{}] @ offset {}"
                   .format(msg.topic(), msg.partition(), msg.offset()))
 
-    for n in range(1000000):
-        record_key = "AJ"
-        record_value = json.dumps({'count': n})
+    n_events = 5000
+    
+    startAt = time.time()
+    
+    for n in range(n_events):
+        record_key = "customer"
+        mock_customer_data = CustomerDataProducer()
+        record_value = json.dumps(mock_customer_data.generateCustomer)
         print("Producing record: {}\t{}".format(record_key, record_value))
         producer.produce(topic, key=record_key, value=record_value, on_delivery=acked)
         producer.poll(0)
-
+        
     producer.flush()
 
     print("{} messages were produced to topic {}!".format(delivered_records, topic))
+    print(f"Execution Time: {time.time() - startAt}")
